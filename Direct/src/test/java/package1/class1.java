586f61745c3d05d7a9caabfe3680cff552a9a5f6
pandu30 <pandu30@gmail.com>
2019-01-20 19:20:55 +0500

    add classes

---
+++ Direct/src/test/java/package1/class1.java
@@ -0,0 +1,13 @@
+package package1;
+
+public class class1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Direct/src/test/java/package2/class1.java
@@ -0,0 +1,13 @@
+package package2;
+
+public class class1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
